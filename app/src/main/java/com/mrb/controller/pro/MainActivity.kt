fd3a55ab38d4a5e1a7efe2950622fe5189e2a057
Mrb-Official <mrb40315@gmail.com>
2026-03-18 09:13:22 +0530

    Fix: Aligned Package names and Manifest to stop crash

--- app/src/main/java/com/mrb/controller/pro/MainActivity.kt
+++ app/src/main/java/com/mrb/controller/pro/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Ye line layout file ko connect karti hai
         setContentView(R.layout.activity_main)
     }
 }
